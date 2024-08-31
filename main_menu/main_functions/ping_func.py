1c052bd486ff7aab0aaf481d56b94ce5ad3ea412
Василий Приказчиков <prikazchikov_v@kazgorset.ru>
2024-08-31 20:45:42 +0300

    ping_func_fix_1

--- main_menu/main_functions/ping_func.py
+++ main_menu/main_functions/ping_func.py
@@ -7,11 +7,16 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from ipaddress import IPv4Address, AddressValueError
 import time
+
 router = Router(name=__name__)
 
 
 class Ping_state(StatesGroup):
     ping_state = State()
+
+
+def check(host):
+    return subprocess.check_output(f'ping -c 1 {host}', shell=True)
 
 
 def ping(ip):
@@ -28,25 +33,38 @@ async def ping_main_fc(callback: CallbackQuery, state: FSMContext):
     await state.clear()
     await callback.answer(cache_time=1)
     await state.set_state(Ping_state.ping_state)
-    await callback.message.answer('Напишите IP до которого нужно проверить доступ по протоколу ICMP:')
+    await callback.message.answer('Напишите Хост до которого нужно проверить доступ по протоколу ICMP:')
 
 
 @router.message(Ping_state.ping_state, ~Command('help', 'start', 'get_id', 'chat_gpt'))
 async def ping_fc(message: Message, state: FSMContext):
     try:
-        ip = IPv4Address(message.text)
-        if ip.is_global:
+        await message.bot.send_chat_action(chat_id=message.chat.id,
+                                           action=ChatAction.TYPING)
+        check(message.text)
+        try:
+            if IPv4Address(message.text).is_global:
+                await message.bot.send_chat_action(chat_id=message.chat.id,
+                                                   action=ChatAction.TYPING)
+                time.sleep(4)
+
+                if ping(message.text):
+
+                    await message.answer(f'{message.text} - <b>Доступен</b> ✅')
+                else:
+                    await message.answer(f'{message.text} - <b>Не доступен</b> ❌')
+            else:
+                await message.answer(f'IP-Адрес не входит в диапазон <b>Белых IP</b>', )
+        except AddressValueError:
             await message.bot.send_chat_action(chat_id=message.chat.id,
                                                action=ChatAction.TYPING)
             time.sleep(4)
-            if ping(ip):
 
-                await message.answer(f'{ip} - <b>Доступен</b> ✅')
+            if ping(message.text):
+
+                await message.answer(f'{message.text} - <b>Доступен</b> ✅')
             else:
-                await message.answer(f'{ip} - <b>Не доступен</b> ❌')
-        else:
-            await message.answer(f'IP-Адрес не входит в диапазон <b>Белых IP</b>',)
-
-    except AddressValueError:
-        await message.answer('Не верный формат IP')
+                await message.answer(f'{message.text} - <b>Не доступен</b> ❌')
+    except subprocess.CalledProcessError:
+        await message.answer('Не правильная запись хоста')
     await state.clear()
