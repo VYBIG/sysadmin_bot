f1487d9428d211e788b5a3c6dc36636eec39faca
Василий Приказчиков <prikazchikov_v@kazgorset.ru>
2024-12-08 20:02:31 +0300

    new_func_testing

--- common_functions.py
+++ common_functions.py
@@ -23,11 +23,21 @@ def main_log(message=None, callback=None, pool=None):
             f'user_full_name={user_full_name}, '
             f'user_message={user_message}, '
             f'username = {username}')
-    else:
+    elif pool is not None:
         user_id = pool.user.id
         user_full_name = pool.user.first_name
         user_message = pool.option_ids
         username = pool.user.username
+        logging.info(
+            f'time={time.asctime()}, user_id={user_id}, '
+            f'user_full_name={user_full_name}, '
+            f'user_message={user_message}, '
+            f'username = {username}')
+    elif message.file_id is not None:
+        user_id = message.from_user.id
+        user_full_name = message.from_user.full_name
+        user_message = message.file_id
+        username = message.from_user.username
         logging.info(
             f'time={time.asctime()}, user_id={user_id}, '
             f'user_full_name={user_full_name}, '
---
+++ main_menu/main_functions/any_file_converter.py
@@ -0,0 +1,3 @@
+def video_for_circle(video):
+    # Video for circle is 400X400 and long is <= 1min
+    pass
